e755fa3782a5c485b3da610a835cb32fe66cc59a
K <x00.x7f@gmail.com>
2017-01-12 17:08:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+echo-static - New cli tool.
+====
+
+# SYNOPSIS
+
+    import "github.com/Code-Hex/echo-static"
+
+# DESCRIPTION
+
+echo-static is ...
+
+# INSTALLATION
+
+    go get github.com/Code-Hex/echo-static
+
+# AUTHOR
+
+[Code-Hex](https://github.com/Code-Hex) <x00.x7f@gmail.com>
---
+++ static.go
@@ -0,0 +1,64 @@
+package static
+
+import (
+	"net/http"
+	"strings"
+
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/labstack/echo"
+)
+
+const version = "0.0.1"
+
+type ServeFileSystem interface {
+	http.FileSystem
+	Exists(prefix string, path string) bool
+}
+type binaryFileSystem struct {
+	fs http.FileSystem
+}
+
+func (b *binaryFileSystem) Open(name string) (http.File, error) {
+	return b.fs.Open(name)
+}
+
+func (b *binaryFileSystem) Exists(prefix string, filepath string) bool {
+	if p := strings.TrimPrefix(filepath, prefix); len(p) < len(filepath) {
+		if _, err := b.Open(p); err != nil {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func BinaryFileSystem(fs *assetfs.AssetFS) *binaryFileSystem {
+	return &binaryFileSystem{fs}
+}
+
+func ServeRoot(urlPrefix string, fs *assetfs.AssetFS) echo.MiddlewareFunc {
+	return serve(urlPrefix, BinaryFileSystem(fs))
+}
+
+// Static returns a middleware handler that serves static files in the given directory.
+func serve(urlPrefix string, fs ServeFileSystem) echo.MiddlewareFunc {
+	fileserver := http.FileServer(fs)
+	if urlPrefix != "" {
+		fileserver = http.StripPrefix(urlPrefix, fileserver)
+	}
+	return func(before echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := before(c)
+			if err != nil && err.Error() != http.StatusText(http.StatusNotFound) {
+				return err
+			}
+
+			w, r := c.Response(), c.Request()
+			if fs.Exists(urlPrefix, r.URL.Path) {
+				fileserver.ServeHTTP(w, r)
+				return nil
+			}
+			return c.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		}
+	}
+}
